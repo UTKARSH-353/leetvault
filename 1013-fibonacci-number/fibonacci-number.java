4a0836ac62ef47a69b6aae58b1a70e7e8ea72628
Utkarsh Kumar <utkarshkumarmth1122@gmail.com>
2025-12-15 11:08:02 +0530

    Time: 9 ms (34.93%) | Memory: 42 MB (46.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1) return n;  
-        return fib(n - 1) + fib(n - 2);
+        if (n <= 1) return n;  //base case
+        return fib(n - 1) + fib(n - 2);//recursive case
     }
 }
 
