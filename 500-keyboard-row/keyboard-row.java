c750fb47771c31f8148f1a979ca8bb28014b8628
Utkarsh Kumar <utkarshkumarmth1122@gmail.com>
2025-12-20 22:16:58 +0530

    Time: 1 ms (34.10%) | Memory: 43 MB (8.47%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        Set<Character> row1 = new HashSet<>(Arrays.asList(
+            'q','w','e','r','t','y','u','i','o','p'));
+        Set<Character> row2 = new HashSet<>(Arrays.asList(
+            'a','s','d','f','g','h','j','k','l'));
+        Set<Character> row3 = new HashSet<>(Arrays.asList(
+            'z','x','c','v','b','n','m'));
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            String lowerWord = word.toLowerCase();
+            if (canBeTyped(lowerWord, row1) || 
+                canBeTyped(lowerWord, row2) || 
+                canBeTyped(lowerWord, row3)) {
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+    private boolean canBeTyped(String word, Set<Character> row) {
+        for (char c : word.toCharArray()) {
+            if (!row.contains(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
