3e9291808def528727a27694576d4a2f040ed57a
Utkarsh Kumar <utkarshkumarmth1122@gmail.com>
2026-02-14 23:08:54 +0530

    Time: 59 ms (86.59%) | Memory: 72.5 MB (66.46%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.java
+++ 901-advantage-shuffle/advantage-shuffle.java
@@ -1,27 +1,22 @@
-import java.util.*;
-
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int n = nums1.length;
+        int[] result = new int[n];
         Arrays.sort(nums1);
-        int[][] nums2Index = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            nums2Index[i][0] = nums2[i];
-            nums2Index[i][1] = i;
-        }
-        Arrays.sort(nums2Index, (a, b) -> a[0] - b[0]);
-
-        int[] res = new int[n];
-        int low = 0, high = n - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            // Compare from largest nums2
-            if (nums1[high] > nums2Index[i][0]) {
-                res[nums2Index[i][1]] = nums1[high--];
+        Integer[] index = new Integer[n];
+        for (int i = 0; i < n; i++) index[i] = i;
+        Arrays.sort(index, (a, b) -> nums2[a] - nums2[b]);
+        int left = 0;
+        int right = n - 1;
+        for (int num : nums1) {
+            if (num > nums2[index[left]]) {
+                result[index[left]] = num;
+                left++;
             } else {
-                res[nums2Index[i][1]] = nums1[low++];
+                result[index[right]] = num;
+                right--;
             }
         }
-
-        return res;
+        return result;
     }
 }
