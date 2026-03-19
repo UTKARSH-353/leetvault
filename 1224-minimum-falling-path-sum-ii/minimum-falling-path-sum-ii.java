c71751b6a39aaff74b5740ae95817c3522b4df54
Utkarsh Kumar <utkarshkumarmth1122@gmail.com>
2026-03-19 14:19:23 +0530

    Time: 38 ms (47.94%) | Memory: 52.5 MB (92.34%) - LeetSync

---
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minFallingPathSum(int[][] grid) {
+        int n = grid.length;
+        int dp[][] = new int[n][n];
+
+        for(int j = 0; j<n; j++){
+            dp[0][j] = grid[0][j];
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<n; j++){
+                int min = Integer.MAX_VALUE;
+                for(int k = 0; k<n; k++){
+                    if(k!=j){
+                        min = Math.min(min, dp[i-1][k]);
+                    }
+                }
+                dp[i][j] = grid[i][j] + min;
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int j = 0; j<n; j++){
+            ans =Math.min(ans, dp[n-1][j]);
+        }
+        return ans;
+    }
+}
